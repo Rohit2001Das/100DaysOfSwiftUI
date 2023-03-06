e6bf5d691db4a76080d4b12c0ad2c3971bc48437
Rohit Das <das2001rohit@gmail.com>
2023-03-06 11:27:07 +0530

    First Commmit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by ROHIT DAS on 05/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
